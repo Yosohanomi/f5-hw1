1c2cf591a4abbd9fd99b5a8f79f2fc6263d3be02
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2025-09-12 19:00:29 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import worldcat from './worldcat.jpg';
 import './App.css';
+const greeting = {
+  name: 'Sofia',
+  text: "Ласкаво просимо до нашого сайту!",
+  subtextImg: 'cat with world'
+}
+
+const website = {
+  name: 'react',
+  url: 'https://legacy.reactjs.org/'
+}
+const firstNum = 6;
+const secondNum = 10;
+
+const colors =  ["Червоний", "Синій", "Зелений"];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="thumb">
+      <h1 className="title">{greeting.name}</h1>
+      <p className="text">{greeting.text}</p>
+      <img src={worldcat} className="image" alt={greeting.subtextImg} />
+      <a href={website.url} className="link">{website.name}</a>
+      <p className="number">{firstNum}+{secondNum}={firstNum + secondNum}</p>
+      <ul className="list">
+        {
+          colors.map(color => {
+            return <li className="list__item" data-color={color.toLocaleLowerCase()}>{color}</li>
+          })
+        }
+      </ul>
+      </div>
     </div>
   );
 }
